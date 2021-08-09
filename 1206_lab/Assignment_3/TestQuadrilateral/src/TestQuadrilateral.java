60823390d39235d0c801a4e002f4ac9a8de0cd0f
Parvez Ahammed <63257806+piru72@users.noreply.github.com>
2021-08-09 22:16:14 +0600

    java assignment done

--- 1206_lab/Assignment_3/TestQuadrilateral/src/TestQuadrilateral.java
+++ 1206_lab/Assignment_3/TestQuadrilateral/src/TestQuadrilateral.java
@@ -5,19 +5,27 @@ public class TestQuadrilateral {
     static Scanner scanner=new Scanner(System.in);
     public static void main(String[] args) throws Exception {
 
-        System.out.println("EInput 1 value: ");
-        double side_s =scanner.nextDouble();
+        System.out.println("Input 1 value: ");
+        double Square1 =scanner.nextDouble();
         
         System.out.println("Input 2 value: ");
-        double side_r =scanner.nextDouble();
-        double base_r =scanner.nextDouble();
+        double Rectangle1 =scanner.nextDouble();
+        double Rectangle2 =scanner.nextDouble();
 
         System.out.println("Input 3 value: ");
-        double side_t1 =scanner.nextDouble();
-        double side_t2 =scanner.nextDouble();
-        double height_t =scanner.nextDouble();
+
+        // double[]Trapezium={};
+
+        // for (int i =0 ;i < 3 ;i ++)
+        //     {
+        //         Trapezium[i] = scanner.nextDouble();
+        //     }
+        double Trapezium1 =scanner.nextDouble();
+        double Trapezium2 =scanner.nextDouble();
+        double Trapezium3 =scanner.nextDouble();
 
 
+        // Quadrilateral res1, res2 , res3 = new Quadrilateral();
         Quadrilateral res1 = new Quadrilateral();
         Quadrilateral res2 = new Quadrilateral();
         Quadrilateral res3 = new Quadrilateral();
@@ -25,16 +33,17 @@ public class TestQuadrilateral {
         Quadrilateral obj1 = new Quadrilateral();
         CreateQuadrilateral obj2 = new CreateQuadrilateral(obj1);
 
+        String Area = " Area: ";
 
-        res1 = obj2.generateQuad(side_s);
-        System.out.println(res1.getType()+" Area: "+ res1.getQuadArea());
 
-        res2 = obj2.generateQuad(side_r, base_r);
-        System.out.println(res2.getType()+" Area: "+ res2.getQuadArea());
+        res1 = obj2.generateQuad(Square1);
+        System.out.println(res1.getType()+ Area + res1.getQuadArea());
 
-        res3 = obj2.generateQuad(side_t1, side_t2, height_t);
-        System.out.println(res3.getType()+" Area: "+ res3.getQuadArea());
+        res2 = obj2.generateQuad(Rectangle1, Rectangle2);
+        System.out.println(res2.getType()+ Area + res2.getQuadArea());
 
+        res3 = obj2.generateQuad(Trapezium1, Trapezium2, Trapezium3);
+        System.out.println(res3.getType()+ Area + res3.getQuadArea());
 
         System.out.println("Hello, World!");
     }
